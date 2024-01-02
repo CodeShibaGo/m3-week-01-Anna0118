32019f68d61787875b3c488f5e92c80886285055
Anna0118 <annachu0988@gmail.com>
2024-01-02 21:51:50 +0800

    id35 test ok

--- find_capitals.py
+++ find_capitals.py
@@ -1,2 +1,14 @@
 def find_capitals(word):
-    pass
+    ls=[]
+    for i in word:
+        if i == i.upper() and i.isalpha():
+            ls.append(i)
+    return ''.join(ls)
+
+# def find_capitals(word):
+#     ls = ''
+#     for i in word:
+#         if i.isupper():
+#             ls += i
+#     return ls
+
