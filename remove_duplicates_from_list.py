da8a0cd2f107278fa7c1716b51c773cc23a5b02f
Anna0118 <annachu0988@gmail.com>
2024-01-03 22:01:00 +0800

    id44 test ok

--- remove_duplicates_from_list.py
+++ remove_duplicates_from_list.py
@@ -1,2 +1,8 @@
 def distinct(seq):
-    pass
+    # 原本還想複製陣列來做檢查和移除，但會遇到index超出範圍的問題
+    # 原來陣列也可用in來掃描，以及用append方式，以及去比對
+    result=[]
+    for i in seq:
+        if len(result)==0 or i!=result[-1]:
+            result.append(i)
+    return result
